a6816e9f5a7b8dfd40dec4561920a70537806375
Micheal Espinola <michealespinola@users.noreply.github.com>
2023-06-26 13:41:46 -0700

    Added multi-subreddit use, fixed dropped exclusion names
    1. Added ability to comma-separate multiple subreddits to download
    1. Fixed dropped wiki page exclusion names

--- reddit.wikidownloader.py
+++ reddit.wikidownloader.py
@@ -5,15 +5,15 @@ import html2text
 
 # Parse command-line arguments
 parser = argparse.ArgumentParser()
-parser.add_argument("subreddit", help="Name of the subreddit")
+parser.add_argument("subreddits", help="Comma-separated list of subreddit names")
 parser.add_argument("reddit_instance", help="Name of the Reddit instance (PRAW) as specified in praw.ini")
 parser.add_argument("twoFA", nargs="?", default=None, help="Optional two-factor authentication code")
 args = parser.parse_args()
 
-sub_name = args.subreddit
+subreddits = args.subreddits.split(',')
 reddit_instance_name = args.reddit_instance
 
-def save_wiki_pages(sub_name, reddit_instance_name):
+def save_wiki_pages(subreddits, reddit_instance_name):
     # Connect to Reddit API using praw.ini file
     reddit = praw.Reddit(reddit_instance_name)
 
@@ -28,52 +28,53 @@ def save_wiki_pages(sub_name, reddit_instance_name):
             user_agent=reddit.config.user_agent
         )
 
-    subreddit = reddit.subreddit(sub_name)
+    for subreddit_name in subreddits:
+        subreddit = reddit.subreddit(subreddit_name)
 
-    # Create a directory to save the wiki pages
-    output_directory = sub_name + '_wiki'
-    os.makedirs(output_directory, exist_ok=True)
+        # Create a directory to save the wiki pages
+        output_directory = subreddit_name + '_wiki'
+        os.makedirs(output_directory, exist_ok=True)
 
-    # Define the dictionary of wiki pages to exclude from conversion
-    excluded_wiki_pages = {
-        "config/stylesheet": "html",  # HTML subreddit stylesheet
-        "usernotes": "json",          # JSON Toolbox per-user notes
-    }
+        # Define the dictionary of wiki pages to exclude from conversion
+        excluded_wiki_pages = {
+            "config/stylesheet": "html",  # HTML subreddit stylesheet
+            "usernotes": "json",          # JSON Toolbox per-user notes
+        }
 
-    # Fetch all wiki pages
-    wiki_pages = subreddit.wiki
+        # Fetch all wiki pages
+        wiki_pages = subreddit.wiki
 
-    for page in wiki_pages:
-        # Check if the page is excluded from conversion
-        if page.name in excluded_wiki_pages:
-            # Save the wiki page without conversion
-            output_extension = excluded_wiki_pages[page.name]
-            output_path = os.path.join(output_directory, page.name + '.' + output_extension)
-            with open(output_path, 'w', encoding='utf-8') as f:
-                f.write(page.content_md)
-        else:
-            # Check if the page has HTML content
-            if page.content_html:
-                # Convert content to markdown format using html2text
-                content_md = html2text.html2text(page.content_html)
+        for page in wiki_pages:
+            # Check if the page is excluded from conversion
+            if page.name in excluded_wiki_pages:
+                # Save the wiki page without conversion
+                output_path = os.path.join(output_directory, page.name + '.' + excluded_wiki_pages[page.name])
+                with open(output_path, 'w', encoding='utf-8') as f:
+                    f.write(page.content_md)
             else:
-                # Skip conversion if the page has no HTML content
-                continue
+                # Check if the page has HTML content
+                if page.content_html:
+                    # Convert content to markdown format using html2text
+                    content_md = html2text.html2text(page.content_html)
+                else:
+                    # Skip conversion if the page has no HTML content
+                    continue
 
-            # Check if the page is a nested page
-            if '/' in page.name:
-                # Create subfolders if needed
-                subfolder = os.path.join(output_directory, os.path.dirname(page.name))
-                os.makedirs(subfolder, exist_ok=True)
-                output_path = os.path.join(subfolder, os.path.basename(page.name) + '.md')
-            else:
-                output_path = os.path.join(output_directory, page.name + '.md')
+                # Check if the page is a nested page
+                if '/' in page.name:
+                    # Create subfolders if needed
+                    subfolder = os.path.join(output_directory, os.path.dirname(page.name))
+                    os.makedirs(subfolder, exist_ok=True)
+                    output_path = os.path.join(subfolder, os.path.basename(page.name) + '.md')
+                else:
+                    output_path = os.path.join(output_directory, page.name + '.md')
 
-            # Save the wiki page as a markdown document
-            with open(output_path, 'w', encoding='utf-8') as f:
-                f.write(content_md)
+                # Save the wiki page as a markdown document
+                with open(output_path, 'w', encoding='utf-8') as f:
+                    f.write(content_md)
 
-        print(f'Saved: {output_path}')
+            print(f'Saved: {output_path}')
+
 
 # Example usage
-save_wiki_pages(sub_name, reddit_instance_name)
+save_wiki_pages(subreddits, reddit_instance_name)
