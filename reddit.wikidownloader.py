646171581619d2d304c6e7cc1df885f1fc8ad35f
Micheal Espinola <michealespinola@users.noreply.github.com>
2023-09-14 12:11:21 -0700

    v3.0.0
    * Added API request limit pausing
    * Added more and better error reporting and trapping
    * Added praw.ini verifications to help troubleshoot issues
    * Added a script runtime timer

--- reddit.wikidownloader.py
+++ reddit.wikidownloader.py
@@ -1,24 +1,48 @@
-import praw
+# Reddit Wiki Downloader
+#
+# Description: Download specific or all joined subreddit wiki documents
+# Version: 3.0.0
+# Author: @michealespinola https://github.com/michealespinola/reddit.wikidownloader
+# Date: September 14, 2023
+
 import argparse
-import os
+import configparser
 import html2text
+import os
+import praw
 import prawcore
 import time
 
+start_time = time.time()
+
 # Parse command-line arguments
 parser = argparse.ArgumentParser()
-parser.add_argument("subreddits_list", help="Comma-separated list of subreddit names")
-parser.add_argument("reddit_instance_name", help="Name of the Reddit instance (PRAW) as specified in praw.ini")
+parser.add_argument("subreddits_list",                help="Comma-separated list of subreddit names")
+parser.add_argument("praw_site_name",                 help="Name of the Reddit site as specified in praw.ini")
 parser.add_argument("twoFA", nargs="?", default=None, help="Optional two-factor authentication code")
 args = parser.parse_args()
 
 subreddits_list = args.subreddits_list.split(',')
-reddit_instance_name = args.reddit_instance_name
+praw_site_name = args.praw_site_name
+
+if not os.path.exists('praw.ini'):
+    print(f"\nError: 'praw.ini' is missing, exiting...\n")
+    exit(1)
+else:
+    # Initialize a configparser object and read the praw.ini file
+    config = configparser.ConfigParser()
+    config.read('praw.ini')
+    if not config.has_section(praw_site_name):
+        print(f"\nError: '{praw_site_name}' site section does not exist in praw.ini, exiting...\n")
+        exit(1)
+
+# Reddit API rate limits max: 10 for non-OAuth clients
+# Reddit API rate limits max: 100 for OAuth clients
+REQUESTS_PER_MINUTE = 90
 
 def login():
     # Connect to Reddit API using praw.ini file
-    reddit = praw.Reddit(reddit_instance_name)
-
+    reddit = praw.Reddit(praw_site_name)
     # Modify the password if the twoFA is provided
     if args.twoFA:
         modified_password = "{}:{}{}".format(reddit.config.password, args.twoFA, '')
@@ -32,17 +56,28 @@ def login():
     return reddit
 
 def get_subreddits(reddit):
-    if args.subreddits_list.upper() == "*JOINED*":
-        # Retrieve the list of subscribed subreddits
-        subreddit_names = [subreddit.display_name for subreddit in reddit.user.subreddits(limit=None)]
-    else:
-        # Manually specified subreddit names
-        subreddit_names = args.subreddits_list.split(',')
+    try:
+        if args.subreddits_list.upper() == "*JOINED*":
+            # Retrieve the list of subscribed subreddits
+            subreddit_names = [subreddit.display_name for subreddit in reddit.user.subreddits(limit=None)]
+        else:
+            # Manually specified subreddit names
+            subreddit_names = args.subreddits_list.split(',')
 
-    sorted_subreddit_names = sorted(subreddit_names, key=str.lower)  # Sort the subreddit names case-insensitively
-    return ','.join(sorted_subreddit_names)
+        sorted_subreddit_names = sorted(subreddit_names, key=str.lower)  # Sort the subreddit names case-insensitively
+        return ','.join(sorted_subreddit_names)
+
+    except prawcore.exceptions.OAuthException as error:
+        print("\r\033[K", end="", flush=True)
+        print(f"Error: PRAW ({error})")
+        print(f"OAUTH: Authorization failed, exiting...\n")
+        exit(1)
 
 def save_wiki_pages(sorted_subreddit_names, reddit):
+
+    # Initialize a timestamp for the start of the current minute and a counter for requests made in the current minute
+    current_minute_start = time.time()
+    requests_in_current_minute = 0
 
     for subreddit_name in sorted_subreddit_names:
         try:
@@ -61,8 +96,36 @@ def save_wiki_pages(sorted_subreddit_names, reddit):
 
             # Fetch all wiki pages
             wiki_pages = subreddit.wiki
+            requests_in_current_minute += 1
 
             for page in wiki_pages:
+                # Check if the current minute has elapsed since the last request
+                current_time = time.time()
+                elapsed_seconds = current_time - current_minute_start
+
+                if elapsed_seconds >= 60:
+                    # Reset the counter and timestamp for the new minute
+                    current_minute_start = current_time
+                    requests_in_current_minute = 0
+
+                # Check if we have made 60 requests in the current minute
+                if requests_in_current_minute >= REQUESTS_PER_MINUTE:
+                    # Calculate the remaining time in the current minute
+                    remaining_time = 60 - elapsed_seconds
+                    # Format the remaining_time to display with no decimal places
+                    remaining_time_formatted = "{:.0f}".format(remaining_time)
+                    print(f"PAUSE: Reached {REQUESTS_PER_MINUTE} API requests per minute (wait {remaining_time_formatted}s)", end="", flush=True)
+                    time.sleep(1)
+
+                    for i in range(int(remaining_time), 0, -1):
+                        print("\r\033[K", end="", flush=True)
+                        print(f"PAUSE: Reached {REQUESTS_PER_MINUTE} API requests per minute (wait {i}s)", end="", flush=True)
+                        time.sleep(1)
+
+                    current_minute_start = time.time()
+                    requests_in_current_minute = 0
+                    print("\r\033[K", end="", flush=True)
+
                 # Check if the page is excluded from conversion
                 if page.name in excluded_wiki_pages:
                     # Save the wiki page without conversion
@@ -102,17 +165,35 @@ def save_wiki_pages(sorted_subreddit_names, reddit):
 
                     print(f'Saved: {output_path}')
 
+                # Increment the counter for requests in the current minute
+                requests_in_current_minute += 1
+
         except prawcore.exceptions.Forbidden as error:
-            print(f"Error: /r/{subreddit_name}/wiki, {error}")
+            print(f"Error: /r/{subreddit_name}/wiki, (HTTP 403: Forbidden)")
+            requests_in_current_minute += 1
 
-#       time.sleep(1)  # Pause for 1 second
+        except prawcore.exceptions.TooManyRequests as error:
+            print(f"Error: /r/{subreddit_name}/wiki, (HTTP 429: Too Many Requests)")
+            print(f"LIMIT: Per minute rate limit exceeded, exiting...\n")
+            exit(1)
 
+        except Exception as error:
+            print(f"Error: {error}")
 
 # Run it
+print(f"\nAuthenticating...", end="", flush=True)
 reddit = login()
+print("\r\033[K", end="", flush=True)
+
+print(f"Loading subreddits...", end="", flush=True)
 subreddits = get_subreddits(reddit)
+print("\r\033[K", end="", flush=True)
 
-print("Scanning Subreddits:")
-print(subreddits)
-
+print(f"Scanning Subreddits: {subreddits}")
+print(f"API ratelimit: {REQUESTS_PER_MINUTE} requests per minute\n")
+# exit()
 save_wiki_pages(subreddits.split(','), reddit)
+
+end_time = time.time()
+elapsed_time = end_time - start_time
+print(f"Script took {elapsed_time:.2f} seconds to run.")
